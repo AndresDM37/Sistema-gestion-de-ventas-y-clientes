8dc14a4f17b94478861a0a77c88713f1dd09b773
andresmarchenaporras@gmail.com <Andres37DM>
2024-05-21 18:20:39 -0500

    ventas

--- app.js
+++ app.js
@@ -1,48 +1,75 @@
-import express from "express"
-import bodyParser from "body-parser"
-import mysql from 'mysql2'
+import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql2";
 
-const app = express()
+const app = express();
 
 const config = {
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: 'A1rnd2are3ps',
-    database: 'sistemadistribuido'
-}
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "A1rnd2are3ps",
+  database: "sistemadistribuido",
+};
 
-const connection = mysql.createConnection(config)
+const connection = mysql.createConnection(config);
 
 connection.connect((err) => {
-    if(err) {
-        console.log('Error connecting to database', err)
-        return
+  if (err) {
+    console.log("Error connecting to database", err);
+    return;
+  }
+  console.log("Connection established");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT ?? 1234;
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  const { nombre, cedula, cargo, departamento } = req.body;
+
+  const sql =
+    "INSERT INTO Cliente (nombre, cedula, cargo, departamento) VALUES (?, ?, ?, ?)";
+  connection.query(
+    sql,
+    [nombre, cedula, cargo, departamento],
+    (err, result) => {
+      if (err) {
+        console.log("Error inserting into database", err);
+        return;
+      }
+
+      res.sendFile(process.cwd() + "/views/venta.html");
+      const idCliente = result.insertId;
     }
-    console.log('Connection established')
-})
+  );
+});
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.post("/submitVenta", (req, res, idCliente) => {
+  const { fechaVenta, descripcionVenta, totalVenta } = req.body;
 
-const PORT = process.env.PORT ?? 1234
+  const sqlVenta =
+    "INSERT INTO Venta (FK_Id_cliente, fecha_de_la_venta, descripcion_de_la_venta, total_de_la_venta) VALUES (?, ?, ?, ?)";
+  connection.query(
+    sqlVenta,
+    [idCliente, fechaVenta, descripcionVenta, totalVenta],
+    (err, result) => {
+      if (err) {
+        console.error("Error insertando datos en Venta:", err);
+        res.send("Error insertando datos en Venta");
+        return;
+      }
 
-app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html')
-})
+      res.send("Datos insertados correctamente en Cliente y Venta");
+    }
+  );
+});
 
-app.post('/submit', (req, res) => {
-    const { nombre, cedula, cargo, departamento } = req.body
-
-    const sql = 'INSERT INTO Cliente (nombre, cedula, cargo, departamento) VALUES (?, ?, ?, ?)'
-    connection.query(sql, [nombre, cedula, cargo, departamento], (err, result) => {
-        if(err) {
-            console.log('Error inserting into database', err)
-            return
-        }
-        res.send('Form send successfully')
-    })
-})
-
-app.listen(PORT, () =>{
-    console.log(`Server is running on port http://localhost:${PORT}`)
-})  
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
