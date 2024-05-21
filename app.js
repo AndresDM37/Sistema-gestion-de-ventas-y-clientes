cd33933e3d201de99bb202cc2e3df3edc593a0b4
andresmarchenaporras@gmail.com <Andres37DM>
2024-05-21 18:29:02 -0500

    ventas

--- app.js
+++ app.js
@@ -31,7 +31,15 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit", (req, res) => {
-  const { nombre, cedula, cargo, departamento } = req.body;
+  const {
+    nombre,
+    cedula,
+    cargo,
+    departamento,
+    fechaVenta,
+    descripcionVenta,
+    totalVenta,
+  } = req.body;
 
   const sql =
     "INSERT INTO Cliente (nombre, cedula, cargo, departamento) VALUES (?, ?, ?, ?)";
@@ -44,31 +52,24 @@ app.post("/submit", (req, res) => {
         return;
       }
 
-      res.sendFile(process.cwd() + "/views/venta.html");
       const idCliente = result.insertId;
+
+      const sqlVenta =
+        "INSERT INTO Venta (FK_Id_cliente, fecha_de_la_venta, descripcion_de_la_venta, total_de_la_venta) VALUES (?, ?, ?, ?)";
+      connection.query(
+        sqlVenta,
+        [idCliente, fechaVenta, descripcionVenta, totalVenta],
+        (err, result) => {
+          if (err) {
+            console.log("Error inserting into database", err)
+            return
+          }
+          res.send('<h1>Data insert correctly</h1>')
+        }
+      );
     }
   );
-});
-
-app.post("/submitVenta", (req, res, idCliente) => {
-  const { fechaVenta, descripcionVenta, totalVenta } = req.body;
-
-  const sqlVenta =
-    "INSERT INTO Venta (FK_Id_cliente, fecha_de_la_venta, descripcion_de_la_venta, total_de_la_venta) VALUES (?, ?, ?, ?)";
-  connection.query(
-    sqlVenta,
-    [idCliente, fechaVenta, descripcionVenta, totalVenta],
-    (err, result) => {
-      if (err) {
-        console.error("Error insertando datos en Venta:", err);
-        res.send("Error insertando datos en Venta");
-        return;
-      }
-
-      res.send("Datos insertados correctamente en Cliente y Venta");
-    }
-  );
-});
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
